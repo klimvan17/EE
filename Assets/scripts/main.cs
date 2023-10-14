e6b022e909efb0e6e69996517309b415ab944e4b
Klivan <vanklim777@gmail.com>
2023-10-14 20:44:41 +0300

    zrabiy dobrae dela

---
+++ Assets/scripts/main.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class UnitMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 6;
+    [SerializeField] private float _jumpForce = 7;
+    private Rigidbody2D _body;
+    [SerializeField] private GroundCheck _ground;
+
+    public float MoveInput { get; set; }
+    public bool OnGround => _ground.OnGround;
+
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _body.velocity = new Vector2(_moveSpeed * MoveInput, _body.velocity.y);
+    }
+}
